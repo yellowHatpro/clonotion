a061bbc4611bd5768590728ee0d7cee1edfda829
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-19 19:42:25 +0530

    themes added, migrations done, added shadcn ui, added catppuccin color scheme and next themes

---
+++ src/lib/providers/next_theme_provider.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import {ThemeProviderProps} from "next-themes/dist/types";
+import {ThemeProvider as NextThemesProvider} from 'next-themes'
+
+export function ThemeProvider({children, ...props}:ThemeProviderProps) {
+    return <NextThemesProvider {...props}>
+        {children}
+    </NextThemesProvider>
+}
