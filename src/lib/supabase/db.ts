196b27eba3530014b5984c9e10664b19b6f71a76
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-19 13:22:25 +0530

    basic setup

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,20 @@
+import postgres from "postgres";
+import * as schema from "../../../migrations/schema"
+import {drizzle} from "drizzle-orm/postgres-js";
+import {migrate} from "drizzle-orm/postgres-js/migrator";
+if (!process.env.DATABASE_URL){
+    console.log("No db URL")
+}
+const migrationClient = postgres(process.env.DATABASE_URL as string, {max:1})
+const db = drizzle(migrationClient,{schema})
+const migrateDB = async () => {
+    try {
+        console.log("Migrating Client")
+        await migrate(db, {migrationsFolder: 'migrations'})
+        console.log("Migration Successful")
+    } catch (e) {
+        console.log("Error migrating client")
+    }
+}
+migrateDB()
+export default db
